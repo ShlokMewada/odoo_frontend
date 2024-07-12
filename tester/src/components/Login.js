1162f752a125ebae3f9868e6ef692daa3b9e6456
ShlokMewada <shlokmewada@gmail.com>
2024-07-12 18:56:07 +0530

    login

--- tester/src/components/Login.js
+++ tester/src/components/Login.js
@@ -2,20 +2,25 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
   const navigate = useNavigate();
-  const [alertMessage, setAlertMessage] = useState();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("https://odoo.detrace.systems/users/api/token/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-      });
+      const response = await fetch(
+        "https://odoo.detrace.systems/users/api/token/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        }
+      );
 
       const data = await response.json();
 
@@ -25,6 +30,8 @@ const Login = () => {
         localStorage.removeItem("redirectAfterLogin");
         navigate(redirectPath);
       } else {
+        alert(data.detail);
+
         // Handle login error
         console.error("Login failed");
         // You might want to show an error message to the user
@@ -36,21 +43,34 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="text"
-        value={credentials.username}
-        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
-        placeholder="Username"
-      />
-      <input
-        type="password"
-        value={credentials.password}
-        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-        placeholder="Password"
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div className="login-container">
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            value={credentials.username}
+            onChange={(e) =>
+              setCredentials({ ...credentials, username: e.target.value })
+            }
+            placeholder="Username"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            value={credentials.password}
+            onChange={(e) =>
+              setCredentials({ ...credentials, password: e.target.value })
+            }
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
