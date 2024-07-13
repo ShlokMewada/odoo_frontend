7247e759da176feb4d089bd934af39d8704b8540
Rudradatt Dave <rudradattdave@gmail.com>
2024-07-13 14:59:58 +0000

    removed unneccessary files and codes

--- tester/src/components/Header.js
+++ tester/src/components/Header.js
@@ -1,114 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "../Header.css";
-// import { Link } from "react-router-dom";
-// import { UserContext } from "./UserContext";
-// import { useContext } from "react";
-
-// const Header = () => {
-//   const navigate = useNavigate();
-//   const [message, setMessage] = useState("");
-//   const [isLoading, setIsLoading] = useState(true);
-//   const { user, setUser } = useContext(UserContext);
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   const fetchData = async () => {
-//     setIsLoading(true);
-//     try {
-//       const response = await fetch(
-//         "https://odoo.detrace.systems/api/users/protected/",
-//         {
-//           method: "GET",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-//           },
-//         }
-//       );
-//       if (response.status === 403) {
-//         // Unauthorized, redirect to login
-//         localStorage.setItem("redirectAfterLogin", window.location.pathname);
-//         // navigate("/login");
-//         return;
-//       }
-
-//       const json = await response.json();
-//       setMessage(json.message);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//       // If there's a network error or other issues, you might want to show an error message
-//       setMessage("An error occurred while fetching data.");
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
-
-//   const handleLogout = () => {
-//     localStorage.removeItem("access_token");
-//     setUser(null);
-//     // navigate("/login");
-//   };
-
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     // <div>
-//     //   <button onClick={handleLogout}>Logout</button>
-//     //   <h1>{message}</h1>
-//     // </div>
-
-//     <header className="header">
-//       <div className="top-bar">
-//         <nav className="top-nav">
-//           {console.log(user)}
-//           {user != null ? (
-//             <div onClick={handleLogout} className="header-div">
-//               logout
-//             </div>
-//           ) : (
-//             <div>
-//               <Link to="/login">login</Link>/<Link to="/signup">signup</Link>
-//             </div>
-//           )}
-//         </nav>
-//         <div className="logo">Dummy</div>
-//         {user != null ? (
-//           <div className="right-nav">
-//             <div className="user">Hello {user.name}</div>
-//           </div>
-//         ) : (
-//           ""
-//         )}
-//       </div>
-//       <div className="search-bar">
-//         <input
-//           type="text"
-//           className="search-input"
-//           placeholder="Search millions of photos, fonts, graphics, and more..."
-//         />
-//       </div>
-//       <nav className="bottom-nav">
-//         <a href="">Home</a>
-//         <Link to="/userprofile">UserProfile</Link>
-//         <a href="">About Us</a>
-//         <a href="">Contact Us</a>
-//         <a href="">Cart</a>
-//         <div className="more-dropdown">
-//           <a href="#more">More</a>
-//         </div>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../Header.css";
@@ -129,11 +18,11 @@ const Header = () => {
     }
     return null;
   };
-  
+
   // Example usage
-  const storedUser = getObjectFromLocalStorage('user');
+  const storedUser = getObjectFromLocalStorage("user");
   console.log(storedUser);
-  const [userData,setUserData] = useState(storedUser)
+  const [userData, setUserData] = useState(storedUser);
 
   useEffect(() => {
     fetchData();
@@ -172,8 +61,8 @@ const Header = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("access_token");
-    localStorage.removeItem("user")
-    setUserData(null)
+    localStorage.removeItem("user");
+    setUserData(null);
     // navigate("/login");
   };
 
@@ -231,4 +120,3 @@ const Header = () => {
 };
 
 export default Header;
-
--- tester/src/components/UserProfile.js
+++ tester/src/components/UserProfile.js
@@ -1,101 +1,3 @@
-// import React, { useState } from "react";
-// import { UserContext } from "./UserContext";
-// import { useContext } from "react";
-
-// const UserProfile = () => {
-//   const { user, setUser } = useContext(UserContext);
-
-//   //   const handleChange = (e) => {
-//   //     const { username ,name, email} = e.target.value;
-//   //     setUser({
-
-//   //     });
-//   //   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     // console.log("User Profile:", user);
-//     // setUser(user)
-
-//     try {
-//       const response = await fetch(
-//         "https://odoo.detrace.systems/api/users/update_profile/",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-//           },
-//           body: JSON.stringify(user),
-//         }
-//       );
-
-//       const data = await response.json();
-
-//       if (response.ok) {
-//         //   localStorage.setItem("access_token", data.tokens.access);
-//         //   setUser(data.user);
-//         //   const redirectPath = localStorage.getItem("redirectAfterLogin") || "/";
-//         //   localStorage.removeItem("redirectAfterLogin");
-//         //   navigate(redirectPath);
-//       } else {
-//         //   alert(data.detail);
-
-//         // Handle login error
-//         console.error("Login failed");
-//         // You might want to show an error message to the user
-//       }
-//     } catch (error) {
-//       console.error("Login error:", error);
-//       // Handle network errors
-//     }
-//   };
-
-//   return (
-//     <div style={{ padding: "20px" }}>
-//       <h2>User Profile</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label>Username: </label>
-//           <input
-//             type="text"
-//             name="username"
-//             value={user.username}
-//             onChange={(e) => {
-//               setUser((user.username = e.target.value));
-//             }}
-//           />
-//         </div>
-//         <div>
-//           <label>Name: </label>
-//           <input
-//             type="text"
-//             name="name"
-//             value={user.name}
-//             onChange={(e) => {
-//               setUser((user.name = e.target.value));
-//             }}
-//           />
-//         </div>
-//         <div>
-//           <label>Email: </label>
-//           <input
-//             type="email"
-//             name="email"
-//             value={user.email}
-//             onChange={(e) => {
-//               setUser((user.email = e.target.value));
-//             }}
-//           />
-//         </div>
-//         <button type="submit">Save Profile</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default UserProfile;
-
 import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from "./UserContext";
 import { useNavigate } from "react-router-dom";
@@ -112,11 +14,11 @@ const UserProfile = () => {
     }
     return null;
   };
-  
+
   // Example usage
-  const storedUser = getObjectFromLocalStorage('user');
+  const storedUser = getObjectFromLocalStorage("user");
   console.log(storedUser);
-  const { user, setUser } = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext);
 
   useEffect(() => {
     fetchUserProfile();
@@ -138,7 +40,7 @@ const UserProfile = () => {
 
       if (response.ok) {
         localStorage.setItem("user", JSON.stringify(data.user)); // Update user context with new data
-        setUser(data.user)
+        setUser(data.user);
         setFormData(data.user);
       } else if (response.status === 403) {
         localStorage.setItem("redirectAfterLogin", window.location.pathname);
@@ -182,8 +84,8 @@ const UserProfile = () => {
 
       if (response.ok) {
         localStorage.setItem("user", JSON.stringify(data.user));
-         // Update user context with new data
-         setUser(data.user)
+        // Update user context with new data
+        setUser(data.user);
         setFormData(data.user);
         alert("Profile updated successfully!");
       } else if (response.status === 403) {
