441604ef3d130f8792c30c1fb30b8226d2a523b6
ShlokMewada <shlokmewada@gmail.com>
2024-07-14 09:44:21 +0530

    bookcard half ready

--- tester/src/components/BookCards.js
+++ tester/src/components/BookCards.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const BookCards = () => {
   return (
-    <div className="flex">
-      
+    <div className="flex gap-2">
+      <img src="" alt="" />
+      <div className="flex flex-col gap-1">
+        <h1></h1>
+        <div className="flex"><p></p><p></p><p>No preview</p></div>
+        <p></p>
+      </div>
     </div>
   )
 }
